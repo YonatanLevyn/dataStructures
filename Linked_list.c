9d6c2ae02a1026ca32b771d5b5eeac704e9477eb
tapuz804 <64014488+tapuz804@users.noreply.github.com>
2023-01-25 19:58:06 +0200

    Update Linked_list.c

--- Linked_list.c
+++ Linked_list.c
@@ -7,7 +7,7 @@ typedef struct node {
    node* next;
 }node;
 
-node* start = NULL;
+node* start = NULL; // maybe change to head
 
 void insert(int value)
 {
@@ -41,14 +41,14 @@ void insert(int value)
 
 int search_by_index(int index)
 {
-     node *tracer, *ptr;
-     for(int i = 0; i < index; i++) { 
-         tracer = ptr;
-         ptr = ptr->next ;
+     node *tracer, *ptr; // you need to make ptr equal to start.
+     for(int i = 0; i < index; i++) { // maybe do something like 
+         tracer = ptr;                // while(index > 0 && ptr != NULL) { index--; ptr = ptr->next; }
+         ptr = ptr->next ;            // and do the if outside of the for.
          if(ptr==NULL)
          {
             printf("nIndex not Found:n");
-            return;
+            return; // you cant do return of nothing if you need to return something.
          }
       }
       return tracer->value;
@@ -57,7 +57,7 @@ int search_by_index(int index)
 /*search for first appearance of element*/
 node* search_by_element(int value)
 {
-   node *ptr;
+   node *ptr; //  = start
    while (ptr->value != value)
    {
       ptr = ptr->next;
@@ -65,9 +65,10 @@ node* search_by_element(int value)
    if (ptr == NULL)
    {
       printf("Element not found");
-      return;
-   }else{
-      
+      return; // need to return something
+   }
+   else
+   {  
       return ptr;
    }
 }
@@ -78,8 +79,10 @@ void delete_by_index(int index)
         if(start==NULL)
         {
                 printf("nThe List is Empty:n");
-                exit(0);
-        }else{
+                exit(0); // no need to exit just return
+        }
+        else
+        {
                 if(index == 0)
                 {
                         /*save pointer to be able to free the memory later*/
@@ -93,14 +96,14 @@ void delete_by_index(int index)
                         ptr = start;
                         for(int i = 0; i < index; i++) { 
                            tracer = ptr;
-                           ptr = ptr->next ;
+                           ptr = ptr->next;
                                 if(ptr==NULL)
                                 {
                                         printf("nIndex not Found:n");
                                         return;
                                 }
                         }
-                        tracer->next = ptr->next;
+                        tracer->next = ptr->next; // nice.
                         printf("nThe deleted element is:%dt", ptr->value );
                         free(ptr);
                 }
